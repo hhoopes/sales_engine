6aaee9d23b2ed6255f7327481cba45287ef83599
hhoopes <heidih@gmail.com>
2016-04-11 23:38:48 -0600

    Add merchant endpoints for index,show,find
    Added tests, controllers, and routes for index,show,find. Trying to make the
    find query dynamic rather than explicitly have to specify it for each resource.
    Also need to make it case-insensitive and test for that.

---
+++ spec/requests/api/v1/merchants/index_merchants_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "Merchants API" do
+  it "returns all merchants" do
+    create_list(:merchant, 20)
+    get '/api/v1/merchants'
+    json = JSON.parse(response.body)
+
+    expect(response).to be_success
+binding.pry
+    expect(json.length).to eq(20)
+    expect(json.last["name"]).to eq(Merchant.last.name)
+ end
+end
