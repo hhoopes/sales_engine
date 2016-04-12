b340de4932ccdbdb6a33a3d7604c1d04a0340360
hhoopes <heidih@gmail.com>
2016-04-12 08:37:47 -0600

    Fix conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-<<<<<<< HEAD
       get 'merchants/find_all'
-=======
->>>>>>> 6aaee9d23b2ed6255f7327481cba45287ef83599
       get 'merchants/find'
       resources :merchants, except: [:new, :edit]
       resources :customers, except: [:new, :edit]
